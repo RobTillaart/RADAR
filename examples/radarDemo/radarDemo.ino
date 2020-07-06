8e5599f0cee8147abcbac0e026a08fabede51bfe
rob tillaart <rob.tillaart@gmail.com>
2020-07-06 23:50:26 +0200

    0.1.3 version

---
+++ examples/radarDemo/radarDemo.ino
@@ -0,0 +1,146 @@
+//
+//    FILE: radarDemo.ino
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.1
+// PURPOSE: demo of the radar library
+//    DATE: 29-03-2015
+//     URL: https://github.com/RobTillaart/RADAR
+
+#include "radar.h"
+
+uint32_t del = 150;
+
+RADAR radar(10, 11);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.print("Start radarDemo, lib version: ");
+  Serial.println(RADAR_LIB_VERSION);
+  Serial.println();
+
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  test6();
+  test7();
+
+  gohome();
+  Serial.println();
+  Serial.print(millis());
+  Serial.println("\tdone...");
+}
+
+void loop()
+{
+}
+
+void test1()
+{
+  Serial.print(millis());
+  Serial.println("\t1. home position");
+  radar.setHomePosition(0, 0);
+  radar.gotoHomePosition();
+  wait();
+}
+
+void test2()
+{
+  Serial.print(millis());
+  Serial.println("\t2. gotoPan 20");
+  radar.gotoPan(20);
+  wait();
+  Serial.println();
+}
+
+void test3()
+{
+  Serial.print(millis());
+  Serial.println("\t3. gotoTilt 60");
+  radar.gotoTilt(60);
+  wait();
+  Serial.println();
+}
+
+void test4()
+{
+  Serial.print(millis());
+  Serial.println("\t4. gotoPanTilt 60 20");
+  radar.gotoPanTilt(60, 20);
+  wait();
+  Serial.println();
+}
+
+void test5()
+{
+  Serial.print(millis());
+  Serial.println("\t5. home position");
+  radar.gotoHomePosition();
+  wait();
+  Serial.println();
+}
+
+void test6()
+{
+  Serial.print(millis());
+  Serial.println("\t6. setPosition");
+  for (int i = 0; i < 10; i++)
+  {
+    radar.setPosition(i, random(90), random(90));
+  }
+  Serial.println();
+}
+
+void test7()
+{
+  Serial.print(millis());
+  Serial.println("\t7. gotoPosition");
+  for (int i = 0; i < 10; i++)
+  {
+    radar.gotoPosition(i);
+    int16_t p, t;
+    radar.getPosition(i, p, t);
+    Serial.print(millis());
+    Serial.print("\t");
+    Serial.print(i);
+    Serial.print("\t");
+    Serial.print(p);
+    Serial.print("\t");
+    Serial.println(t);
+
+    wait();
+    Serial.println();
+  }
+}
+
+void gohome()
+{
+  Serial.print(millis());
+  Serial.println("\tgo home position");
+  radar.gotoHomePosition();
+  wait();
+  Serial.println();
+}
+
+void wait()
+{
+  Serial.println("\nTIME\t\tPAN\tTILT");
+  do
+  {
+    Serial.print(millis());
+    Serial.print("\t*\t");
+    Serial.print(radar.getPan());
+    Serial.print("\t");
+    Serial.println(radar.getTilt());
+    delay(del);
+  }  while (radar.isMoving());
+  Serial.print(millis());
+  Serial.print("\t*\t");
+  Serial.print(radar.getPan());
+  Serial.print("\t");
+  Serial.println(radar.getTilt());
+}
+
+// -- END OF FILE --
---
+++ radar.h
@@ -0,0 +1,72 @@
+#pragma once
+//
+//    FILE: radar.h
+//  AUTHOR: Rob Tillaart
+// VERSION: see RADAR_LIB_VERSION
+// PURPOSE: Arduino library for a pan tilt radar.
+//     URL: https://github.com/RobTillaart/RADAR
+
+#include "Arduino.h"
+
+#define RADAR_LIB_VERSION      "0.1.3"
+
+#ifndef RADAR_POSITIONS
+#define RADAR_POSITIONS       10
+#endif
+
+class RADAR
+{
+public:
+    RADAR(const uint8_t, const uint8_t);
+
+    // no valid range checking or negative value check.
+    void     setPanPerSecond(float pps) { _panPerSecond = pps; };
+    float    getPanPerSecond() { return _panPerSecond; };
+    void     setTiltPerSecond(float tps) { _tiltPerSecond = tps; };
+    float    getTiltPerSecond() { return _tiltPerSecond; };
+
+    // basic moves
+    void     gotoPan(const int16_t pan);
+    int16_t  getPan();
+    void     gotoTilt(const int16_t tilt);
+    int16_t  getTilt();
+    void     gotoPanTilt(const int16_t pan, const int16_t tilt);
+
+    // memory positions - store / recall?
+    uint8_t  getMaxPositions() { return RADAR_POSITIONS; };
+    bool     setPosition(const uint8_t idx, const int16_t pan, const int16_t tilt);
+    bool     getPosition(const uint8_t idx, int16_t & pan, int16_t & tilt);
+    bool     gotoPosition(const uint8_t idx);
+    void     setHomePosition(const int16_t pan, const int16_t tilt);
+    void     gotoHomePosition();
+
+    // feedback on positions.
+    bool     isMoving() { return isPanMoving() || isTiltMoving(); };
+    bool     isPanMoving() { return getPan() != _pan; };
+    bool     isTiltMoving() { return getTilt() != _tilt; };
+
+    uint32_t ping();
+    uint32_t ping(const int16_t pan, const int16_t tilt);
+    
+private:
+    int16_t   _pinPan;
+    int16_t   _pinTilt;
+    
+    int16_t   _prevPan;
+    int16_t   _pan;
+    int16_t   _homePan;
+    uint32_t  _lastPanTime;
+    
+    int16_t   _prevTilt;
+    int16_t   _tilt;
+    int16_t   _homeTilt;
+    uint32_t  _lastTiltTime;
+    
+    int16_t   _panArray[RADAR_POSITIONS];
+    int16_t   _tiltArray[RADAR_POSITIONS];
+
+    float     _panPerSecond = 15;
+    float     _tiltPerSecond = 15;
+};
+
+// -- END OF FILE --
